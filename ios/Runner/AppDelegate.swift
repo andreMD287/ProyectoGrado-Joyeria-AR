1e275d3f5252c7683a239156d022dbc613798653
JoseOG0208 <glasssubtendedcat@gmail.com>
2026-08-23 17:04:51 -0500

    feat(C2): portar tracking de muñeca en iOS a producción
    Integra Apple Vision (HandTrackingHandler) al target de Xcode, corrige inconsistencias de entorno (CocoaPods, deployment target, SPM) y un crash fatal al detener el tracking en iOS. Validado en iPhone físico.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+  private var handTracking: HandTrackingHandler?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,5 +14,7 @@ import UIKit
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+    handTracking = HandTrackingHandler(
+      messenger: engineBridge.applicationRegistrar.messenger())
   }
 }
